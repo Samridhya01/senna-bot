48c324c6acd6d562bbb968e773370e5f3d020f6f
Samridhya Santra <125868472+Samridhya01@users.noreply.github.com>
2024-05-06 10:45:48 +0530

    Update config.js

--- config.js
+++ config.js
@@ -1,52 +1,80 @@
 import { watchFile, unwatchFile } from 'fs'
-import chalk from 'chalk' 
-import { fileURLToPath } from 'url' 
+import chalk from 'chalk'
+import { fileURLToPath } from 'url'
+import fs from 'fs'
 
-global.owner = [
-  ['59172945992', 'FG98', true],
-  ['5491168352204']
-] //Numeros de owner 
+global.owner = [['919062628928', 'its Samriddho', true], ['15204130681', 'Samrat Xd', true], ['']] //Number of owners
 
-global.mods = [''] 
-global.prems = ['50489079501', '573143917092']
-global.APIs = { // API Prefix
-  // name: 'https://website' 
+//global.pairingNumber = "" //put your bot number here
+
+global.mods = ['919062628928', '15204130681']
+global.prems = ['919062628928', '15204130681', '919339619072']
+global.allowed = ['919062628928', '15204130681', '19152999993']
+global.keysZens = ['c2459db922', '37CC845916', '6fb0eff124']
+global.keysxxx = keysZens[Math.floor(keysZens.length * Math.random())]
+global.keysxteammm = [
+  '29d4b59a4aa687ca',
+  '5LTV57azwaid7dXfz5fzJu',
+  'cb15ed422c71a2fb',
+  '5bd33b276d41d6b4',
+  'HIRO',
+  'kurrxd09',
+  'ebb6251cc00f9c63',
+]
+global.keysxteam = keysxteammm[Math.floor(keysxteammm.length * Math.random())]
+global.keysneoxrrr = ['5VC9rvNx', 'cfALv5']
+global.keysneoxr = keysneoxrrr[Math.floor(keysneoxrrr.length * Math.random())]
+global.lolkeysapi = ['GataDios']
+
+global.APIs = {
+  // API Prefix
+  // name: 'https://website'
+  xteam: 'https://api.xteam.xyz',
+  dzx: 'https://api.dhamzxploit.my.id',
+  lol: 'https://api.lolhuman.xyz',
+  violetics: 'https://violetics.pw',
+  neoxr: 'https://api.neoxr.my.id',
+  zenzapis: 'https://zenzapis.xyz',
+  akuari: 'https://api.akuari.my.id',
+  akuari2: 'https://apimu.my.id',
   nrtm: 'https://fg-nrtm.ddns.net',
-  fgmods: 'https://api.fgmods.xyz'
+  bg: 'http://bochil.ddns.net',
+  fgmods: 'https://api-fgmods.ddns.net',
 }
-global.APIKeys = { // APIKey Here
+global.APIKeys = {
+  // APIKey Here
   // 'https://website': 'apikey'
-  'https://api.fgmods.xyz': 'm2XBbNvz' //-- 100 de límite diario --- Regístrese en https://api.fgmods.xyz/
+  'https://api.xteam.xyz': 'd90a9e986e18778b',
+  'https://api.lolhuman.xyz': '85faf717d0545d14074659ad',
+  'https://api.neoxr.my.id': `${keysneoxr}`,
+  'https://violetics.pw': 'beta',
+  'https://zenzapis.xyz': `${keysxxx}`,
+  'https://api-fgmods.ddns.net': 'fg-dylux',
 }
 
 // Sticker WM
-global.packname = 'Senna┃ᴮᴼᵀ' 
-global.author = '@fg98' 
+global.botname = '˜”°•✩•°”˜ 𝚂𝚊𝚖𝚛𝚊𝚝 𝚇𝙳 𝙱𝚘𝚝 ˜”°•✩•°”˜'
+global.premium = 'true'
+global.packname = 'XD┃ᴮᴼᵀ' 
+global.author = '@Samriddho' 
+global.menuvid = 'https://imgur.com/lBCI9Os.mp4'
+global.igfg = '▢ Follow on Instagram\nhttps://www.instagram.com/Santrasamridhya\n' 
+global.dygp = 'https://chat.whatsapp.com/C4NU6KefgoLL9W8vpbhbEY'
+global.fgsc = 'https://github.com/Samridhya01/GURU-BOT' 
+global.fgyt = 'https://youtube.com/'
+global.fgpyp = 'https://www.buymeacoffee.com/samridhya'
+global.fglog = 'https://raw.githubusercontent.com/Guru322/api/Guru/guru.jpg' 
+global.thumb = fs.readFileSync('./Assets/Gurulogo.jpg')
 
-//--info FG
-global.botName = 'Senna'
-global.fgig = 'https://instagram.com/fg98_ff' 
-global.fgsc = 'https://github.com/FG98F/dylux-fg' 
-global.fgyt = 'https://youtube.com/fg98f'
-global.fgpyp = 'https://paypal.me/fg98f'
-global.fglog = 'https://i.ibb.co/1zdz2j3/logo.jpgs' 
-
-//--- Grupos WA
-global.id_canal = '120363177092661333@newsletter' //-ID de canal de WhatsApp
-global.fgcanal = 'https://whatsapp.com/channel/0029VaCeuZd6mYPQiWqxXj1F'
-global.bgp = 'https://chat.whatsapp.com/BESBo5xjvIZE4YVvth6Yzr'
-global.bgp2 = 'https://chat.whatsapp.com/I7bvd8XCAOUHjgkHteqFC7'
-global.bgp3 = 'https://chat.whatsapp.com/F0JTTyZ3hsoL7OlU8TEpuH' //--GP NSFW
-
-global.wait = '⌛ _Cargando..._\n*▬▬▬▭*'
+global.wait = '*⌛ _Charging..._*\n*▰▰▰▱▱▱▱▱*'
 global.rwait = '⌛'
 global.dmoji = '🤭'
 global.done = '✅'
-global.error = '❌' 
-global.xmoji = '🔥' 
+global.error = '❌'
+global.xmoji = '🔥'
 
-global.multiplier = 69 
-global.maxwarn = '2' // máxima advertencias
+global.multiplier = 69
+global.maxwarn = '3'
 
 let file = fileURLToPath(import.meta.url)
 watchFile(file, () => {

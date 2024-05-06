5cc0c41c9cab1247c233f920a8ecac4f906f9309
Samridhya Santra <125868472+Samridhya01@users.noreply.github.com>
2024-05-06 11:10:20 +0530

    Update main-Alive2.js

--- plugins/main-Alive2.js
+++ plugins/main-Alive2.js
@@ -1,1 +1,50 @@
+let handler = async (m, { conn, text, usedPrefix, command }) => {
+  // Sound
+  let name = m.pushName || conn.getName(m.sender)
+  var vn = 'https://github.com/Samridhya01/senna-bot/raw/main/Assets/mp3/Alive2.mp3'
+  let url = 'https://github.com/Samridhya01'
+  let murl = 'https://youtu.be/'
+  let img = 'https://mcdn.wallpapersafari.com/335/80/4/t5oI4Y.jpg'
+  let con = {
+    key: {
+      fromMe: false,
+      participant: `${m.sender.split`@`[0]}@s.whatsapp.net`,
+      ...(m.chat ? { remoteJid: '16504228206@s.whatsapp.net' } : {}),
+    },
+    message: {
+      contactMessage: {
+        displayName: `${name}`,
+        vcard: `BEGIN:VCARD\nVERSION:3.0\nN:;a,;;;\nFN:${name}\nitem1.TEL;waid=${m.sender.split('@')[0]}:${m.sender.split('@')[0]}\nitem1.X-ABLabel:Ponsel\nEND:VCARD`,
+      },
+    },
+  }
+  let doc = {
+    audio: {
+      url: vn,
+    },
+    mimetype: 'audio/mpeg',
+    ptt: true,
+    waveform: [100, 0, 100, 0, 100, 0, 100],
+    fileName: 'Guru',
 
+    contextInfo: {
+      mentionedJid: [m.sender],
+      externalAdReply: {
+        title: 'I AM ALIVE',
+        body: 'SAMRAT BOT',
+        thumbnailUrl: img,
+        sourceUrl: 'https://chat.whatsapp.com/F3sB3pR3tClBvVmlIkqDJp',
+        mediaType: 1,
+        renderLargerThumbnail: true,
+      },
+    },
+  }
+
+  await conn.sendMessage(m.chat, doc, { quoted: con })
+}
+
+handler.help = ['alive']
+handler.tags = ['main']
+handler.command = /^(alive2)$/i
+
+export default handler
